85fd2069f76e57eb437984069d8362bd89f7dec5
mehwishjbe <mehwish.jbe@gmail.com>
2024-03-10 21:59:32 +0100

    player 2 click enable

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ boxes.forEach((box) => {
     box.addEventListener("click", () => {
         if(turnO){
             box.innerText ="O";
+            turnO = false;
         }else{
             box.innerText ="X";
+            turnO = true;
         }
     });
 });
