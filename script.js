49af0bae1c2bfd120c30f3c07fb058a34edf85b2
mehwishjbe <mehwish.jbe@gmail.com>
2024-03-12 07:39:57 +0100

    editing format updated

--- script.js
+++ script.js
@@ -9,74 +9,73 @@ let titleContainer = document.querySelector(".title-container");
 let turnO = true;
 
 const winPatterns = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8]
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
 ];
 
 boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        if(turnO){
-            box.innerText ="O";
-            turnO = false;
-        }else{
-            box.innerText ="X";
-            turnO = true;
-        }
-        box.disabled = true;
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
 
-        checkWinner();
-    });
+    checkWinner();
+  });
 });
 
 const showWinner = (winner) => {
-    msg.innerText = `Congratulations, Winner is ${winner}`;
-    msgContainer.classList.remove("hide");
-    title.innerText = "";
-    titleContainer.classList.remove("hidden");
-    disableBoxes();
-}
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  title.innerText = "Tic Tac Toe";
+  titleContainer.classList.remove("hidden");
+  disableBoxes();
+};
 
 const disableBoxes = () => {
-    for(let box of boxes){
-        box.disabled = true;
-    }
-}
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
 
 const checkWinner = () => {
-    for(let pattern of winPatterns){
-        let pos1Val   =    boxes[pattern[0]].innerText;
-        let pos2Val   =    boxes[pattern[1]].innerText; 
-        let pos3Val   =    boxes[pattern[2]].innerText;
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
 
-        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
-            if(pos1Val  === pos2Val && pos2Val === pos3Val){
-                console.log("Winner", pos1Val);
-                showWinner(pos1Val);
-            }
-        }
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("Winner", pos1Val);
+        showWinner(pos1Val);
+      }
     }
+  }
 };
 
 const resetGame = () => {
-    turnO = true;
-    enableBoxes();
-    titleContainer.classList.add("hidden");
-    msgContainer.classList.add("hide");
-}
+  turnO = true;
+  enableBoxes();
+  titleContainer.classList.add("hidden");
+  msgContainer.classList.add("hide");
+};
 
 const enableBoxes = () => {
-    for(let box of boxes){
-        box.disabled = false;
-        box.innerText = " ";
-
-    }
-}
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = " ";
+  }
+};
 
 resetBtn.addEventListener("click", resetGame);
 newGameBtn.addEventListener("click", resetGame);
